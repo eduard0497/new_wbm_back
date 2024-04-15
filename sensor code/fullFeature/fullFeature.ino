def92f36c7c03859ce8029d0cc5afd4a5cfe38e2
dchurley <136537343+dchurley@users.noreply.github.com>
2024-04-15 12:28:26 -0700

    Update fullFeature.ino

--- sensor code/fullFeature/fullFeature.ino
+++ sensor code/fullFeature/fullFeature.ino
@@ -2,7 +2,7 @@
 #include "LoRaWanMinimal_APP.h"
 #include "Arduino.h"
 #include <NewPing.h>
-#include <Wire.h>               
+#include <Wire.h>
 #include "HT_SH1107Wire.h"
 //comment this out to disable debug
 #define DEBUG
@@ -13,21 +13,12 @@
 #define ECHO_PIN GPIO1
 #define TRIG_PIN GPIO2
 
-/*
- * set LoraWan_RGB to Active,the RGB active in loraWan
- * RGB red means sending;
- * RGB purple means joined done;
- * RGB blue means RxWindow1;
- * RGB yellow means RxWindow2;
- * RGB green means received done;
- */
-
 //Set these OTAA parameters
 static uint8_t devEui[] = { 0x0e, 0xe6, 0x82, 0xe9, 0x8d, 0xd5, 0x68, 0xec };
 static uint8_t appEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 static uint8_t appKey[] = { 0xf6, 0xe9, 0x2e, 0x34, 0x1a, 0x36, 0x60, 0x80, 0xe1, 0x5c, 0x08, 0xdc, 0xe0, 0x5d, 0xd8, 0x39 };
 
-uint16_t userChannelsMask[6] = { 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+uint16_t userChannelsMask[6] = { 0x00FF, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000 };
 
 //counter for tracking sent packets
 static uint8_t counter = 0;
@@ -40,7 +31,7 @@ float maxVoltage = 4100;
 NewPing sonar(TRIG_PIN, ECHO_PIN, 200);
 //display object for debug display
 #ifdef DEBUG
-SH1107Wire  display(0x3c, 500000, SDA, SCL ,GEOMETRY_128_64,GPIO10); // addr, freq, sda, scl, resolution, rst
+SH1107Wire display(0x3c, 500000, SDA, SCL, GEOMETRY_128_64, GPIO10);  // addr, freq, sda, scl, resolution, rst
 #endif
 
 ///////////////////////////////////////////////////
@@ -49,44 +40,42 @@ TimerEvent_t sleepTimer;
 //Records whether our sleep/low power timer expired
 bool sleepTimerExpired;
 
-static void wakeUp()
-{
-  sleepTimerExpired=true;
+static void wakeUp() {
+  sleepTimerExpired = true;
 }
 
-static void lowPowerSleep(uint32_t sleeptime)
-{
-  sleepTimerExpired=false;
-  TimerInit( &sleepTimer, &wakeUp );
-  TimerSetValue( &sleepTimer, sleeptime );
-  TimerStart( &sleepTimer );
+static void lowPowerSleep(uint32_t sleeptime) {
+  sleepTimerExpired = false;
+  TimerInit(&sleepTimer, &wakeUp);
+  TimerSetValue(&sleepTimer, sleeptime);
+  TimerStart(&sleepTimer);
   //Low power handler also gets interrupted by other timers
   //So wait until our timer had expired
   while (!sleepTimerExpired) lowPowerHandler();
-  TimerStop( &sleepTimer );
+  TimerStop(&sleepTimer);
 }
 
 ///////////////////////////////////////////////////
 void setup() {
   //activate serial
-	Serial.begin(115200);
-  //activate display (most likely debug only)
-  #ifdef DEBUG
+  Serial.begin(115200);
+//activate display (most likely debug only)
+#ifdef DEBUG
   display.init();
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
   display.clear();
   display.drawString(0, 0, "Init Sensor...");
   display.display();
-  #endif
+#endif
 
-  if (ACTIVE_REGION==LORAMAC_REGION_US915) {
+  if (ACTIVE_REGION == LORAMAC_REGION_US915) {
     //TTN uses sub-band 2 in AU915
     LoRaWAN.setSubBand2();
   }
- 
+
   LoRaWAN.begin(LORAWAN_CLASS, ACTIVE_REGION);
-  
+
   //Enable ADR
   LoRaWAN.setAdaptiveDR(true);
 
@@ -106,10 +95,9 @@ void setup() {
 }
 
 ///////////////////////////////////////////////////
-void loop()
-{
+void loop() {
   //Counter is just some dummy data we send for the example
-  counter++; 
+  counter++;
 
 
   //COLLECT SENSOR INFO
@@ -120,7 +108,7 @@ void loop()
 
   //based on 4.100v = 100% and 2.930v = 0%
   //this calculation will produce the battery percentage
-  float percent = (voltage - minVoltage) * 100 / (maxVoltage - minVoltage);
+  float percent = (voltage - minVoltage) * 100.0f / (maxVoltage - minVoltage);
   uint8_t truncated = (uint8_t)percent;
 
   //bundle into a uint8_t array
@@ -129,47 +117,40 @@ void loop()
   data[0] = dist;
   data[1] = truncated;
 
-  //first byte is distance, second and third byte are the two bytes of battery voltage
-  // uint8_t* voltageBytes = (uint8_t*)&voltage;
-  // uint8_t data[3];
-  // data[0] = dist;
-  // data[1] = voltageBytes[0];
-  // data[2] = voltageBytes[1];
-  
-  //In this demo we use a timer to go into low power mode to kill some time.
-  //You might be collecting data or doing something more interesting instead.
-  lowPowerSleep(15000);  
-
   //Now send the data. The parameters are "data size, data pointer, port, request ack"
   //Here we send confirmed packed (ACK requested) only for the first five (remember there is a fair use policy)
   bool requestack = (counter < 5);
   const char* result;
-  if (LoRaWAN.send(3, data, 1, requestack)) {
+  if (LoRaWAN.send(sizeof(data), data, 1, requestack)) {
     result = "SEND OK";
   } else {
     result = "SEND FAILED";
   }
 
+  //create message and print to serial
   char message[40];
-  sprintf(message, "cnt=%d, dst=%d, vlt=%d\n", counter, dist, voltage);
+  sprintf(message, "cnt=%d, dst=%d, vlt=%4.3f\n", counter, dist, voltage);
   Serial.print(message);
   Serial.println(result);
 
-  #ifdef DEBUG
+  //draw message to display
+#ifdef DEBUG
   display.clear();
   display.drawString(0, 0, message);
   display.drawString(0, 10, result);
   display.display();
-  #endif
+#endif
+
+  //Finally low power sleep until next cycle
+  lowPowerSleep(15000);
 }
 
 ///////////////////////////////////////////////////
 //Example of handling downlink data
-void downLinkDataHandle(McpsIndication_t *mcpsIndication)
-{
-  Serial.printf("Received downlink: %s, RXSIZE %d, PORT %d, DATA: ",mcpsIndication->RxSlot?"RXWIN2":"RXWIN1",mcpsIndication->BufferSize,mcpsIndication->Port);
-  for(uint8_t i=0;i<mcpsIndication->BufferSize;i++) {
-    Serial.printf("%02X",mcpsIndication->Buffer[i]);
+void downLinkDataHandle(McpsIndication_t* mcpsIndication) {
+  Serial.printf("Received downlink: %s, RXSIZE %d, PORT %d, DATA: ", mcpsIndication->RxSlot ? "RXWIN2" : "RXWIN1", mcpsIndication->BufferSize, mcpsIndication->Port);
+  for (uint8_t i = 0; i < mcpsIndication->BufferSize; i++) {
+    Serial.printf("%02X", mcpsIndication->Buffer[i]);
   }
   Serial.println();
 }
